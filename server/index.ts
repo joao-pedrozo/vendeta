e5079eda17622b60c854e477c0b0422e0b38af94
P <jp.professional@outlook.com>
2024-03-12 19:23:23 -0300

    chore: setup prisma

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { PrismaClient } from "./prisma/src/generated/client";
+
+const app = express();
+const port = 3000;
+const prisma = new PrismaClient();
+
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
