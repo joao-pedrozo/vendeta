fd6c663a25b985c95a5d2a7329eec196a00fea59
P <jp.professional@outlook.com>
2024-03-12 20:41:40 -0300

    chore: setup tailwind

--- overlay/src/App.tsx
+++ overlay/src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>Hello</h1>;
+  return <h1 className="text-red-700 text-4xl text-center mt-8">Hello</h1>;
 }
 
 export default App;
