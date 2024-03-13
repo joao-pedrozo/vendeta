6851f58701b79850feb81bc2a8f07fa03c694556
P <jp.professional@outlook.com>
2024-03-12 21:48:08 -0300

    feat: add header and footer

--- overlay/src/App.tsx
+++ overlay/src/App.tsx
@@ -2,7 +2,7 @@ import Overlay from "./Overlay";
 
 function App() {
   return (
-    <div className="p-32 flex justify-center">
+    <div className="p-32 flex justify-center bg-black min-h-screen">
       <Overlay />
     </div>
   );
--- overlay/src/Overlay.tsx
+++ overlay/src/Overlay.tsx
@@ -1,7 +1,31 @@
 function Overlay() {
   return (
-    <div className="bg-black text-white flex justify-center w-fit p-4">
-      <span>Hello</span>
+    <div className="bg-[#161324] text-white w-fit p-4 h-[540px] relative">
+      <div className="flex justify-center gap-12 items-center">
+        <img src="./vendeta.png" className="w-10 h-10" />
+        <div className="text-center">
+          <h1 className="font text-xl font-semibold">Points</h1>
+          <h1 className="font text-xl">Leaderboard</h1>
+        </div>
+        <div className="flex items-center flex-col gap-1">
+          <img
+            src="https://wallpapers-clan.com/wp-content/uploads/2023/05/cool-anime-pfp-01.jpg"
+            className="w-8 h-8 rounded-full"
+          />
+          <span className="text-[12px]">230</span>
+        </div>
+      </div>
+      <div className="bg-[#0a0911] absolute bottom-8 left-[80px] rounded-md">
+        <ul className="flex gap-2 p-2 uppercase">
+          <li className="cursor-pointer hover:text-zinc-500 transition-all">
+            Points
+          </li>
+          {"•"}
+          <li className="cursor-pointer hover:text-zinc-500 transition-all">
+            Store
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
